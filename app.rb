da426ee4042c1159530d8682f97618456bc43272
Adam Kovari <adam@kovari.eu>
2026-01-17 19:46:01 +0100

    Fix Rack 3.x compatibility: use Rackup::Handler

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "kiket_sdk"
+require 'rackup'
 require "json"
 require "faraday"
 require "faraday/multipart"
@@ -764,7 +765,7 @@ end
 if __FILE__ == $PROGRAM_NAME
   extension = JiraExtension.new
 
-  Rack::Handler::Puma.run(
+  Rackup::Handler.get(:puma).run(
     extension.app,
     Host: ENV.fetch("HOST", "0.0.0.0"),
     Port: ENV.fetch("PORT", 8080).to_i,
